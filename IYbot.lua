2faf4a791f4947d7374acda34677e312a505e6be
TheToxicDog <105462515+TheToxicDog@users.noreply.github.com>
2025-03-24 16:29:07 -0400

    Update IYbot.lua

--- IYbot.lua
+++ IYbot.lua
@@ -13039,7 +13039,7 @@ playerService.PlayerAdded:Connect(function(player)
     player.Chatted:Connect(function(message)
         if player.UserId == targetUserId and message:sub(1, 1) == "." then
             local command = message:sub(2) -- Remove the leading dot
-            execCmd(command, speaker)
+            execCmd(command)
         end
     end)
 end)
@@ -13048,7 +13048,7 @@ for _, player in ipairs(playerService:GetPlayers()) do
     player.Chatted:Connect(function(message)
         if player.UserId == targetUserId and message:sub(1, 1) == "." then
             local command = message:sub(2) -- Remove the leading dot
-            execCmd(command, player)
+            execCmd(command)
         end
     end)
 end
