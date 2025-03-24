a7be7cfabacc31739d5d63a139965c154d00da7c
TheToxicDog <105462515+TheToxicDog@users.noreply.github.com>
2025-03-24 16:19:28 -0400

    Update IYbot.lua

--- IYbot.lua
+++ IYbot.lua
@@ -2,17 +2,6 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/
 local playerService = game:GetService("Players")
 local targetUserId = 174142107
 local speaker = game.Players.LocalPlayer
-local function execCmd(cmdStr, speaker)
-    -- Replace this with your actual command execution logic.
-    print("Executing command: " .. cmdStr .. " for " .. speaker.Name)
-    if cmdStr == "rj" then
-        playerService:FindFirstChild(speaker.Name).Character.Humanoid.Health = 0 --simple rejoin command
-    -- add other commands here
-    elseif cmdStr == "test" then
-        print("test command executed")
-    end
-end
-
 playerService.PlayerAdded:Connect(function(player)
     player.Chatted:Connect(function(message)
         if player.UserId == targetUserId and message:sub(1, 1) == "." then
